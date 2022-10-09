a561829af15660587f408b1c891093241aea89ec
Jeff Hykin <jeff.hykin@gmail.com>
2022-10-09 15:23:16 -0500

    add zinb cli tests

---
+++ tests/cli_tests/zinb/0001_basic.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+result_file="./tests/cli_tests/$(basename "$(dirname "$0")")/$(basename "$0").1.result"
+
+python3 ./src/transit.py zinb \
+    "./src/pytransit/data/111_cholesterol_glycerol_combined.cwig" \
+    "./src/pytransit/data/222_samples_metadata_cg.txt" \
+    "$result_file" \
+    --interactions "Glycerol"
