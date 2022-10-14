5549bd3eab7ff76014aaf340be443af706a03fc7
Jeff Hykin <jeff.hykin@gmail.com>
2022-10-14 14:00:41 -0500

    -

--- tests/cli_tests/pathway_enrichment/0003_example.sh
+++ tests/cli_tests/pathway_enrichment/0003_example.sh
@@ -7,4 +7,10 @@ comwig="./src/pytransit/data/cholesterol_glycerol_combined.dat"
 DATA="src/pytransit/data/"
 
 # can also do GSEA method (on any system of functional categories)
-transit pathway_enrichment "tests/cli_tests/resampling/0001_basic.sh.1.result" $DATA/H37Rv_sanger_roles.dat $DATA/sanger_roles.dat "$result_file" -M GSEA
+transit pathway_enrichment \
+    "tests/cli_tests/resampling/0001_basic.sh.1.result" \
+    $DATA/H37Rv_sanger_roles.dat \
+    $DATA/sanger_roles.dat \
+    "$result_file" \
+    --lfc_col 6 \
+    -M GSEA
