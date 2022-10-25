dd64bfd3781d7c9c4049f166c2ba4eb97211efd8
Sanjeevani Choudhery <schoudhery@csce-bwgsh03.engr.tamu.edu>
2022-10-25 18:49:39 -0500

    fixes to test for corrplot

--- tests/cli_tests/corrplot/0002_basic_err.sh
+++ tests/cli_tests/corrplot/0002_basic_err.sh
@@ -5,4 +5,4 @@ annotation="./src/pytransit/data/genomes/H37Rv.prot_table"
 metadata="./src/pytransit/data/samples_metadata_cg.txt"
 comwig="./src/pytransit/data/cholesterol_glycerol_combined.dat"
 
-transit corrplot ./src/pytransit/data/cholesterol_glycerol_combined.dat "$result_file"
+python3 ./src/transit.py corrplot ./src/pytransit/data/cholesterol_glycerol_combined.dat "$result_file"
