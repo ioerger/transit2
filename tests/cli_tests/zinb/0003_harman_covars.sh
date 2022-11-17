724a63ce8452c2196dfd3c244599683b10be3334
Jeff Hykin <jeff.hykin@gmail.com>
2022-11-17 13:37:34 -0600

    update zinb tests

---
+++ tests/cli_tests/zinb/0003_harman_covars.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+result_file="./tests/cli_tests/$(basename "$(dirname "$0")")/$(basename "$0").1.result"
+annotation="./src/pytransit/data/genomes/mc2_155_tamu.prot_table"
+metadata="./src/pytransit/data/harman_randomized.metadata.tsv"
+comwig="./src/pytransit/data/harman_randomized.comwig.tsv"
+
+annotation="./src/pytransit/data/mc2_155_tamu.prot_table"
+
+# Strains: "DKO" "rel" "WT" "x5849"
+# Time Slices: 0days, 7days, 14days, 28days
+
+# asking about "KO_REL" condition
+python3 ./src/transit.py zinb \
+    "$comwig" \
+    "$annotation" \
+    "$metadata" \
+    "$result_file" \
+    -include-conditions WT_Starved_d28,rel_Starved_d28,5849_Starved_d28,DKO_Starved_d28 \
+    -condition "KO_rel" \
+    -interactions KO_5849 \
+    -gene uvrA
