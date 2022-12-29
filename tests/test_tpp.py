6e425a7232146e4d6c738adfbabde866adc83612
Jeff Hykin <jeff.hykin@gmail.com>
2022-12-29 18:08:59 -0500

    handle no bwa better

--- tests/test_tpp.py
+++ tests/test_tpp.py
@@ -59,8 +59,11 @@ def get_bwa():
         return "/usr/local/bin/bwa"
     elif platform.system() != "Windows":
         import subprocess
-        output = subprocess.check_output([ 'sh', '-c', 'command -v bwa'])
-        return output.decode()[0:-1]
+        try:
+            output = subprocess.check_output([ 'sh', '-c', 'command -v bwa'])
+            return output.decode()[0:-1]
+        except Exception as error:
+            pass
     return ""
 
 bwa_path = get_bwa()
