e81a75b5bbefbc3cb65cf0f40aaa3be3167fd211
Jeff Hykin <jeff.hykin@gmail.com>
2023-01-18 10:20:28 -0600

    tests pass

--- tests/cli_tests/hmm/0001_basic.sh
+++ tests/cli_tests/hmm/0001_basic.sh
@@ -5,7 +5,10 @@ annotation="./src/pytransit/data/genomes/H37Rv.prot_table"
 metadata="./src/pytransit/data/cholesterol_glycerol.transit/metadata.tsv"
 comwig="./src/pytransit/data/cholesterol_glycerol.transit/comwig.tsv"
 
+
 python3 ./src/transit.py hmm \
-    "src/pytransit/data/cholesterol_glycerol.transit/cholesterol_rep3.wig,src/pytransit/data/cholesterol_glycerol.transit/cholesterol_rep2.wig,src/pytransit/data/cholesterol_glycerol.transit/cholesterol_rep1.wig" \
-    ./src/pytransit/data/genomes/H37Rv.prot_table \
+    "$comwig" \
+    "$metadata" \
+    "$annotation" \
+    "Cholesterol" \
     "$result_file"
