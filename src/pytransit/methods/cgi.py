aac76c092b3b8bb9afd2202c29d7b2b21b860b1b
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-22 18:31:08 -0600

    add correct drop down

--- src/pytransit/methods/cgi.py
+++ src/pytransit/methods/cgi.py
@@ -641,11 +641,7 @@ class Method:
     def on_wig_option_click():
         logging.log("You clicked a dropdown option")
 
-    @gui.add_menu("Method", "himar1", menu_name)
-    def on_menu_click(event):
-        Method.define_panel(event)
-
-    @gui.add_menu("Method", "tn5", menu_name)
+    @gui.add_menu("Method", "CRISPRi", menu_name)
     def on_menu_click(event):
         Method.define_panel(event)
 
